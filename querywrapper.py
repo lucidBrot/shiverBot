8d1eeaf0f73267f7b45552d2b6050d0e572f7b37
Eric Mink <eric@mink.li>
2019-08-24 23:45:10 +0200

    need to find better way for escaping quotes

--- querywrapper.py
+++ querywrapper.py
@@ -3,12 +3,14 @@
 # This file is a python2 wrapper for the python3 file query.py
 # YES, this is a bad idea and the python2 files should be migrated to python3 ;)
 import subprocess
+from pipes import quote
 
 def query(email, limit = 50):
+    email = email.replace("'", r"\'").replace('"', r'\"')
     script = [
             "python3.6", 
             "-c", 
-            u"import query; print( query.query_by_email('{email}', {limit}) )".format(email = email, limit = limit)
+                u"import query; print( query.query_by_email('{email}', {limit}) )".format(email = email, limit = limit)
         ]
-    proc = subprocess.check_output(script, shell=False)
+    proc = subprocess.check_output(script, shell=False) 
     return proc
