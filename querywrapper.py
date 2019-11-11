1f2bdc076797487a200a5acd285be6bf6cc260ad
Eric Mink <eric@mink.li>
2019-11-11 17:55:18 +0100

    Could not access the database at this time

--- querywrapper.py
+++ querywrapper.py
@@ -3,14 +3,18 @@
 # This file is a python2 wrapper for the python3 file query.py
 # YES, this is a bad idea and the python2 files should be migrated to python3 ;)
 import subprocess
+from subprocess import CalledProcessError
 from pipes import quote
 
 def query(email, limit = 50):
-    email = email.replace('\\', '\\\\').replace("'", r"\'").replace('"', r'\"')
-    script = [
-            "python3.6", 
-            "-c", 
-                u"import query; print( query.query_by_email('{email}', {limit}) )".format(email = email, limit = limit)
-        ]
-    proc = subprocess.check_output(script, shell=False) 
-    return proc
+    try:
+        email = email.replace('\\', '\\\\').replace("'", r"\'").replace('"', r'\"')
+        script = [
+                "python3.6", 
+                "-c", 
+                    u"import query; print( query.query_by_email('{email}', {limit}) )".format(email = email, limit = limit)
+            ]
+        proc = subprocess.check_output(script, shell=False) 
+        return proc
+    except CalledProcessError as e:
+        return "Could not access the database at this time."
